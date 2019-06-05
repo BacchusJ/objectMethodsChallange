c3af8b30ccc62cd0704df8b974fccfa26e700fa9
Vesper Jones <vesperjones@Vespers-MacBook-Pro.local>
2019-06-04 22:38:52 -0500

    added comments to functions and if statements

--- johnsMass.js
+++ johnsMass.js
@@ -2,6 +2,8 @@ var mark = {
     fullName: 'Mark Miller',
     height: 1.89,
     weight: 97.1,
+    //adding a function to our object 
+    //the BMI is object's weight devided by their height multiplied by 2
     calcMass: function() {
        this.bmi = this.weight / (this.height * this.height);
        return this.bmi;
@@ -23,14 +25,14 @@ var john = {
 }
 
 
-
+//using our functions for the if and else if statements
 if (john.calcMass() > mark.calcMass()) {
     console.log (mark.fullName + ' \'s BMI of ' + mark.bmi  + ' is higher than John\'s there for, he wins! ');
 
 } else if (john.calcMass() > mark.calcMass()) {
     console.log (john.fullName + ' \'s BMI of ' + john.bmi  + ' is higher than John\'s there for, he wins! ');
 }
-
+//in case of a tide
 else {
     console.log('They are both the same');
 }
